506d2f05905551c2a3eea0b6eb9412059001b3fa
Hafsa H <hafsasiddiqui94@gmail.com>
2024-12-17 17:15:21 +0500

    💫 Configure react router and appwrite

---
+++ src/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: "https://cloud.appwrite.io/v1",
+    appwriteProjectId: "675050b80002e91b85da",
+    appwriteDatabaseId: "67571a800016d9df2323",
+    appwriteExpensesCollectionId: "675818880025fbf7ee57",
+    appwriteBudgetsCollectionId: "6758187b003db1e21323",
+  };
+  
+  export default conf;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Homepage from "./components/Homepage.jsx";
+import Login from "./components/Login.jsx";
+import Budgets from "./components/Budgets.jsx";
+import CreateBudget from "./components/CreateBudget.jsx";
+import AddExpense from "./components/AddExpense.jsx";
+import Expenses from "./components/Expenses.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/budgets" element={<Budgets />} />
+        <Route path="/create-budget" element={<CreateBudget />} />
+        <Route path="/expenses" element={<Expenses />}>
+          <Route path="add" element={<AddExpense />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
